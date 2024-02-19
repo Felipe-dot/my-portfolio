019bd3e327a38564e170e920e12b9bcdd49b75ce
Felipe-dot <felipeluis610@gmail.com>
2024-02-19 11:44:05 -0300

    Style portfolio item

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -66,13 +66,19 @@ const PortfolioPage = () => {
                 key={item.id}
               >
                 <div className="flex flex-col gap-8 text-white">
-                  <h1>{item.title}</h1>
-                  <div className="relative">
-                    <Image src={item.image} alt="" fill />
+                  <h1 className="text-xl font-bold md:text-4xl lg:text-6xl xl:text-8xl">
+                    {item.title}
+                  </h1>
+                  <div className="relative w-80 h-56 md:w-96 md:h-64 lg:w-[500px] lg:h-[350px] xl:w-[600px] xl:h-[420px]">
+                    <Image src={item.img} alt="" fill />
                   </div>
-                  <p>{item.desc}</p>
-                  <Link href={item.link}>
-                    <button>See Demo</button>
+                  <p className="w-80 md:w-96 lg:w-[500px] lg:text-lg xl:w-[600px]">
+                    {item.desc}
+                  </p>
+                  <Link href={item.link} className="flex justify-end">
+                    <button className="p-2 text-sm md:p-4 md:text-md lg:p-8 lg:text-lg bg-white text-gray-600 font-semibold m-4 rounded">
+                      See Demo
+                    </button>
                   </Link>
                 </div>
               </div>
