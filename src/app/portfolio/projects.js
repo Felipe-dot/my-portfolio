6af07666c61cfe3d9fbf5998f259afd1bc203000
Felipe-dot <felipeluis610@gmail.com>
2024-02-27 15:38:38 -0300

    Refactor portfolio page

---
+++ src/app/portfolio/projects.js
@@ -0,0 +1,44 @@
+const projects = [
+  {
+    id: 1,
+    color: "from-red-300 to-blue-300",
+    title: "Ocean",
+    desc: "Sistema multiplataforma que lembra os usuários de se hidratarem, o projeto foi feito utilizado Flutter, Javascript para o frontend e Parse server para o backend.",
+    img: "https://i.ibb.co/525twsK/ocean-Mocks.png",
+    link: "https://www.youtube.com/shorts/flzxnzY4R-I",
+  },
+  {
+    id: 2,
+    color: "from-blue-300 to-violet-300",
+    title: "Next.js Todo App",
+    desc: "Desenvolvimento de um Todo List implementado com Next.js e Tailwind CSS, com armazenamento de dados persistente utilizando localStorage.",
+    img: "https://i.ibb.co/0VSgN5H/desktop-preview.jpg",
+    link: "https://next-todo-app-rose-chi.vercel.app/",
+  },
+  {
+    id: 3,
+    color: "from-violet-300 to-purple-300",
+    title: "Sunnyside landing page",
+    desc: "Landing Page desenvolvida utilizando Javascript, Html e CSS, projeto desenvolvido para treinar os conceitos de display flex e grid.",
+    img: "https://i.ibb.co/k92WxVC/desktop-preview.jpg",
+    link: "https://sunnysidewebpage.netlify.app/",
+  },
+  {
+    id: 4,
+    color: "from-purple-300 to-red-300",
+    title: "Promptopia",
+    desc: "Criação de um hub interativo de comandos para ChatBots de IA, desenvolvido com Next.js, Tailwind CSS possuindo integração com Google OAuth e MongoDB.",
+    img: "https://i.ibb.co/3Bf9TR6/Screenshot-from-2024-02-27-14-46-51.png",
+    link: "https://share-prompts-git-main-felipe-dot.vercel.app/",
+  },
+  {
+    id: 5,
+    color: "from-red-300 to-blue-300",
+    title: "Interactive Solar System",
+    desc: "Sistema solar interativo desenvolvido utilizando Three.Js, Javascript e CSS.",
+    img: "https://i.ibb.co/ssw3f0H/solar-demo.gif",
+    link: "https://65a8129953acab18ee74b9cd--creative-strudel-af9952.netlify.app/",
+  },
+];
+
+export default projects;
